7a6a172404232ee840ca9f82abfe830bf13e4b39
Wilz22 <santiagollumiquinga36@gmail.com>
2024-11-23 13:15:12 -0500

    arreglo imagen

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,3 +7,15 @@ export default defineConfig({
   integrations: [tailwind()],
   site: "https://santiagodev.me",
 });
+
+// import { defineConfig } from "astro/config";
+// import tailwind from "@astrojs/tailwind";
+
+// export default defineConfig({
+//   integrations: [tailwind()],
+//   site: "https://santiagodev.me",
+//   server: {
+//     port: 4321,
+//     https: false,
+//   },
+// });
